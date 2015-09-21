d8bcbfd8e20292f9938ce2ca53d01a90e1a71f71
Jérôme Blanchard <jayblanc@gmail.com>
2015-09-21 12:17:59 +0200

    add configuration using java property

--- filestore-ejb/src/main/java/org/filestore/ejb/config/FileStoreConfig.java
+++ filestore-ejb/src/main/java/org/filestore/ejb/config/FileStoreConfig.java
@@ -9,6 +9,8 @@ public class FileStoreConfig {
 	static {
 		if ( System.getenv("FILESTORE_HOME") != null ) {
 	    	home = Paths.get(System.getenv("FILESTORE_HOME"));
+	    } else if ( System.getProperty("filestoreHome") != null ) {
+	    	home = Paths.get(System.getProperty("filestoreHome"));
 	    } else {
 	    	home = Paths.get(System.getProperty("user.home"), ".filestore");
 	    }
