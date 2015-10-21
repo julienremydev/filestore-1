b3e4af316c09e471cb30886759fd0e51793ae722
Jérôme Blanchard <jayblanc@gmail.com>
2015-10-21 15:09:14 +0200

    custom wsdl and xml

--- filestore-api/src/main/java/org/filestore/api/FileItem.java
+++ filestore-api/src/main/java/org/filestore/api/FileItem.java
@@ -4,10 +4,16 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
 
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+@XmlRootElement(name="file")
 public abstract class FileItem implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@XmlAttribute
 	public abstract String getId();
 
 	public abstract String getName();
@@ -20,6 +26,7 @@ public abstract class FileItem implements Serializable {
 	
 	public abstract String getOwner();
 
+	@XmlTransient
 	public abstract List<String> getReceivers();
 
 	public abstract String getMessage();
--- filestore-api/src/main/java/org/filestore/api/FileService.java
+++ filestore-api/src/main/java/org/filestore/api/FileService.java
@@ -7,9 +7,12 @@ import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.Style;
 
 @Remote
 @WebService
+@SOAPBinding(style=Style.RPC)
 public interface FileService {
 	
 	@WebMethod(operationName="postfile")
